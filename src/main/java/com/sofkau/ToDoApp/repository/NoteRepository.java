f2a40698d36bd15f823629990d23e5afd1012ce8
Eslashier <anfelov@gmail.com>
2022-05-03 15:36:34 -0500

    NoteServiceImplementation implements noteService

--- src/main/java/com/sofkau/ToDoApp/repository/NoteRepository.java
+++ src/main/java/com/sofkau/ToDoApp/repository/NoteRepository.java
@@ -2,7 +2,9 @@ package com.sofkau.ToDoApp.repository;
 
 import com.sofkau.ToDoApp.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
 
